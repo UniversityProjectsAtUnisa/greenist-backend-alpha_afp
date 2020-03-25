0d86e53ff0fe70b2d0a110d9decea5e74ecf7da7
marco741 <marco@ninjacampus.io>
2020-03-25 13:48:42 +0100

    fixed start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+cd /home/pi/Greenist_backend;
 source flask/bin/activate; 
 pip install -r requirements.txt; 
 env FLASK_APP=app.py python -m flask run --host 0.0.0.0;
 source deactivate;
+cd -;
