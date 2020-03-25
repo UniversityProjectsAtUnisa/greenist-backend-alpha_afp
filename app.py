ffc09e47f46e00489c3dcda9504b7de0cbcd25ac
marco741 <marco@ninjacampus.io>
2020-03-25 13:09:14 +0100

    app, test and start shell command

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("Hello World")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def hello():
+    return "Hello World!"
---
+++ start.sh
@@ -0,0 +1,1 @@
+ssh -t pi@greenist.ddns.net ''
