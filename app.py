8bbcfe75375053f515ddc995b9f2eed19c776232
marco741 <marco@ninjacampus.io>
2020-03-25 22:53:51 +0100

    Added direct app launch and config from object

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask
+import config
+
 
 app = Flask(__name__)
+
+app.config.from_object('config.DevConfig')
+app.config.from_envvar('~/.env', silent=True)
 
 
 @app.route("/", methods=["GET"])
 def hello():
     return "Hello World!"
+
+
+if __name__ == '__main__':
+    app.run()
