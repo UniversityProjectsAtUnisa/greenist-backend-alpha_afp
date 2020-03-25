229668e79df1fbb5c732bdc332e23ac3ad94fc16
marco741 <marco@ninjacampus.io>
2020-03-25 22:54:19 +0100

    Unified deploy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,5 @@
-ssh -t pi@192.168.1.149 'cd Greenist_backend; git fetch --all; git reset --hard origin/master; python3 -m pip install -r requirements.txt; sudo systemctl restart flask.service;'
+if [ $# = 1 ]; then
+    ssh -t pi@192.168.1.149 'cd Greenist_backend; git fetch --all; git reset --hard origin/master; python3 -m pip install -r requirements.txt; sudo systemctl restart flask.service;'
+else
+    ssh -t pi@greenist.ddns.net -p 2022 "cd Greenist_backend; git fetch --all; git reset --hard origin/master; python3 -m pip install -r requirements.txt; sudo systemctl restart flask.service"
+fi
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/pi/Greenist_backend;
-env FLASK_APP=app.py python3 -m flask run --host 0.0.0.0;
-cd -;
