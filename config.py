6c1ef88567d0182de59dcada27600c112d53ef41
marco741 <marco@ninjacampus.io>
2020-03-25 22:54:07 +0100

    Added config support with .env in home directory

---
+++ config.py
@@ -0,0 +1,25 @@
+"""Flask config class."""
+import os
+from dotenv import load_dotenv
+dotenv_path = os.path.join(os.getenv('HOME'), '.env')  # Path to .env file
+load_dotenv(dotenv_path)
+
+
+class Config:
+    """Base config vars."""
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    SESSION_COOKIE_NAME=os.environ.get('SESSION_COOKIE_NAME')
+    SERVER_NAME=os.environ.get('0.0.0.0')
+
+
+class ProdConfig(Config):
+    DEBUG=False
+    TESTING=False
+    DATABASE_URI=os.environ.get('PROD_DATABASE_URI')
+
+
+class DevConfig(Config):
+    ENV="development"
+    DEBUG=True
+    TESTING=True
+    DATABASE_URI=os.environ.get('DEV_DATABASE_URI')
